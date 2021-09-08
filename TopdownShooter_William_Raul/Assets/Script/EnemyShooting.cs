6020c132a4e447e53dd4d6776654ddae1f7c5c2f
Raul Blanco <rf-blanco1140@users.noreply.github.com>
2021-09-08 16:39:59 +0200

    Enemy shooting Cycle

---
+++ TopdownShooter_William_Raul/Assets/Script/EnemyShooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    private void Start()
+    {
+        StartCoroutine(ShootingCycle());
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    IEnumerator ShootingCycle()
+    {
+        while(true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
