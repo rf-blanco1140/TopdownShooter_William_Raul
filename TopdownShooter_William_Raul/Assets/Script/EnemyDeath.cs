c8726065a773905c3d71cacedd67fa046d31abbd
Raul Blanco <rf-blanco1140@users.noreply.github.com>
2021-09-09 16:47:02 +0200

    Enemy flashes when hit
    theres the edge case in which the player shoot multiple times at the enemy

--- TopdownShooter_William_Raul/Assets/Script/EnemyDeath.cs
+++ TopdownShooter_William_Raul/Assets/Script/EnemyDeath.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    private SpriteColor _spriteColor;
+
+    private void Start()
+    {
+        _spriteColor = GetComponent<SpriteColor>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "PlayerBullet")
@@ -14,6 +21,7 @@ public class EnemyDeath : MonoBehaviour
 
     IEnumerator DeathProcess()
     {
+        _spriteColor.PlaySpriteDamageFlash();
         yield return new WaitForSeconds(0.5f);
         Destroy(this.gameObject);
     }
