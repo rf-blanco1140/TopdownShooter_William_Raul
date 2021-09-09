2b384b5bdfb62360fa68baf5059c2fa50c1a2d66
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-09 13:44:48 +0200

    Bullets destroy after 5 sec

--- TopdownShooter_William_Raul/Assets/Script/Bullet.cs
+++ TopdownShooter_William_Raul/Assets/Script/Bullet.cs
@@ -6,11 +6,14 @@ public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
 
+    public void Update()
+    {
+        Destroy(gameObject, 5f);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1f);
         Destroy(gameObject);
     }
 }
