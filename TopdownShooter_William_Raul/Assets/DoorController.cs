06c7f5addf86b5cb8fe6c5629caa575aefaf8adb
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-10 10:03:32 +0200

    Some code clean up for door controller

--- TopdownShooter_William_Raul/Assets/DoorController.cs
+++ TopdownShooter_William_Raul/Assets/DoorController.cs
@@ -8,22 +8,13 @@ public class DoorController : MonoBehaviour
     public int enemiesLeft = 0;
     bool killedAllEnemies = false;
     public GameObject door;
-    void Start()
-    {
-        enemiesLeft = 10; // or whatever;
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-//        GameObject[] door = GameObject.FindGameObjectsWithTag("Door");
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         enemiesLeft = enemies.Length;
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    enemiesLeft--;
-        //}
+
         if (enemiesLeft > 0)
         {
             door.SetActive(true);
