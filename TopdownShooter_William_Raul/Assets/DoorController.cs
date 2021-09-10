42609f42006e9aef93527a74b154f70864446a45
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-10 09:28:48 +0200

    Door Controller added to prefab

--- TopdownShooter_William_Raul/Assets/DoorController.cs
+++ TopdownShooter_William_Raul/Assets/DoorController.cs
@@ -7,6 +7,7 @@ public class DoorController : MonoBehaviour
     // Start is called before the first frame update
     public int enemiesLeft = 0;
     bool killedAllEnemies = false;
+    public GameObject door;
     void Start()
     {
         enemiesLeft = 10; // or whatever;
@@ -16,15 +17,20 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+//        GameObject[] door = GameObject.FindGameObjectsWithTag("Door");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         enemiesLeft = enemies.Length;
         //if (Input.GetKeyDown(KeyCode.A))
         //{
         //    enemiesLeft--;
         //}
+        if (enemiesLeft > 0)
+        {
+            door.SetActive(true);
+        }
         if (enemiesLeft == 0)
         {
-            Debug.Log("doorOpen");
+            door.SetActive(false);
         }
     }
 }
