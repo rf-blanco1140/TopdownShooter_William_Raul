49cbd167d7d203ace93edc5cfba2e7773b8bb8a7
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-13 16:14:11 +0200

    Movement effect on both wheels

--- TopdownShooter_William_Raul/Assets/Script/TankMovement.cs
+++ TopdownShooter_William_Raul/Assets/Script/TankMovement.cs
@@ -7,6 +7,9 @@ public class TankMovement : MonoBehaviour
     public float moveSpeed = 5f;
     Vector2 movement;
     public GameObject moveEffect;
+    public Transform wheel1;
+    public Transform wheel2;
+    public Transform exhaustPort;
     public Rigidbody2D rb;
 
     // Update is called once per frame
@@ -17,11 +20,13 @@ public class TankMovement : MonoBehaviour
 
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            Instantiate(moveEffect, wheel1.position, Quaternion.identity);
+            Instantiate(moveEffect, wheel2.position, Quaternion.identity);
         }
     }
     void FixedUpdate()
     {
+  //      Instantiate(moveEffect, exhaustPort.position, Quaternion.identity);
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
 }
