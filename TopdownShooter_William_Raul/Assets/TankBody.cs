1b98d914abf2017ee91a7ad67978e909dca83afc
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-13 11:26:01 +0200

    Tank Body Rotation

---
+++ TopdownShooter_William_Raul/Assets/TankBody.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBody : MonoBehaviour
+{
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 300 * Time.deltaTime);
+        }
+    }
+}
---
+++ TopdownShooter_William_Raul/Assets/TankMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Vector2 movement;
+
+    //public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    //}
+}
