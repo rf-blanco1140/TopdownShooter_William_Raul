7a98108097b5c852e5a3a0430ea4992c4acf9377
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-14 10:42:52 +0200

    Wheel tracks and cam shake when moving

--- TopdownShooter_William_Raul/Assets/Script/TankMovement.cs
+++ TopdownShooter_William_Raul/Assets/Script/TankMovement.cs
@@ -12,21 +12,37 @@ public class TankMovement : MonoBehaviour
     public Transform exhaustPort;
     public Rigidbody2D rb;
 
+    public float camShakeMagnitude, camShakeDuration;
+    private CameraFollow _camShake;
+
     // Update is called once per frame
+    private void Start()
+    {
+        _camShake = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
+    }
+
     void Update()
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
 
+    }
+    void FixedUpdate()
+    {
+        //      Instantiate(moveEffect, exhaustPort.position, Quaternion.identity);
+
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             Instantiate(moveEffect, wheel1.position, Quaternion.identity);
             Instantiate(moveEffect, wheel2.position, Quaternion.identity);
+
+            if (!Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(_camShake.Shaking(camShakeDuration, camShakeMagnitude));
+            }
+
         }
-    }
-    void FixedUpdate()
-    {
-  //      Instantiate(moveEffect, exhaustPort.position, Quaternion.identity);
+
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
 }
