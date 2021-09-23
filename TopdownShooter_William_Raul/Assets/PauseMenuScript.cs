2483d5567a5c888086ab5b603f9e485bfb22b826
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-23 10:39:19 +0200

    Pause menu

---
+++ TopdownShooter_William_Raul/Assets/PauseMenuScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    //Menu
+    public GameObject mainMenu;
+    public bool menuOn;
+
+    private Shooting _shooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shooting = GameObject.Find("Hull").GetComponent<Shooting>();
+        menuOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        mainMenu.SetActive(menuOn);
+        if (Input.GetKeyDown(KeyCode.Escape) && !menuOn)
+        {
+            PauseGame();
+
+        }
+        else
+        if (Input.GetKeyDown(KeyCode.Escape) && menuOn)
+        {
+            ResumeGame();
+
+        }
+        if (menuOn)
+        {
+            Cursor.visible = true;
+        }
+        else
+            Cursor.visible = false;
+    }
+
+    void PauseGame()
+    {
+        _shooting.enabled = false;
+        Time.timeScale = 0;
+        menuOn = true;
+        //Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Debug.Log("Escape key was pressed");
+    }
+
+    public void ResumeGame()
+    {
+        _shooting.enabled = true;
+        Time.timeScale = 1;
+        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        menuOn = false;
+        Debug.Log("Escape key was pressed again");
+    }
+}
