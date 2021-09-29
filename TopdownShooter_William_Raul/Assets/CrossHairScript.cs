2424f7f8c3b782b70e8ee271ba20bf3012d18619
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2021-09-29 10:30:35 +0200

    CrossHair

---
+++ TopdownShooter_William_Raul/Assets/CrossHairScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHairScript : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.visible = false;
+    }
+
+
+    public void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+
+        //Set fake mouse Cursor
+        transform.position = mousePos;
+    }
+}
